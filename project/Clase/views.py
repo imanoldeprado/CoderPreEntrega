2f5c4559d347501621e74d287fa7fbe698e2f422
Imanol De Prado <imanol_mj@hotmail.com>
2024-04-29 16:47:13 -0300

    add: terminó el afterclass

--- project/Clase/views.py
+++ project/Clase/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 from . import models
 
 def home(request):
-    ...
+    query = models.Comision.objects.all()
+    context = {"comisiones": query}
+    return render(request, "Clase/index.html", context)
