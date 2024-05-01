a43e1b36514e7c41f26e17779112dc92c5e3db24
Imanol De Prado <imanol_mj@hotmail.com>
2024-05-01 19:03:14 -0300

    creación de formulario en producto

--- project/producto/urls.py
+++ project/producto/urls.py
@@ -6,4 +6,5 @@ app_name = "producto"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("productocategoria/create/", views.productocategoria_create, name="productocategoria_create")
 ]
--- project/producto/views.py
+++ project/producto/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
-from . import models
+from . import models, forms
 
 def home(request):
     consulta_productos = models.ProductoCategoria.objects.all
     context = {"productos": consulta_productos}
     return render(request, "producto/index.html", context)
+
+def productocategoria_create(request):
+    if request.method == "POST":
+        form = forms.ProductoCategoriaForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect("producto:home")
+    else: #request.method == "GET"
+        form = forms.ProductoCategoriaForm()
+    return render(request, "producto/productocategoria_create.html", context= {"form": form})
